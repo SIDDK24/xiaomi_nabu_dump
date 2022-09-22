9c8d319b947d8f0466f117e09067bb42189d2822
SIDDK24 <skoyande17@gmail.com>
2022-09-22 15:22:02 +0000

    Add extras for qssi-user 12 12 SKQ1.220303.001 V13.1.1.0.SKXINXM release-keys
    Signed-off-by: SIDDK24 <skoyande17@gmail.com>

---
+++ join_split_files.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat product/data-app/Photos/Photos.apk.* 2>/dev/null >> product/data-app/Photos/Photos.apk
+rm -f product/data-app/Photos/Photos.apk.* 2>/dev/null
+cat product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null >> product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk
+rm -f product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system_ext/apex/com.android.vndk.v30.apex
+rm -f system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system_ext/priv-app/MiuiSystemUI/MiuiSystemUI.apk.* 2>/dev/null >> system_ext/priv-app/MiuiSystemUI/MiuiSystemUI.apk
+rm -f system_ext/priv-app/MiuiSystemUI/MiuiSystemUI.apk.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat vendor_boot.img.* 2>/dev/null >> vendor_boot.img
+rm -f vendor_boot.img.* 2>/dev/null
+cat vendor_bootimg/05_dtbdump_z$,|`T.dtb.* 2>/dev/null >> vendor_bootimg/05_dtbdump_z$,|`T.dtb
+rm -f vendor_bootimg/05_dtbdump_z$,|`T.dtb.* 2>/dev/null
+cat bootimg/01_dtbdump_gO"PW.dtb.* 2>/dev/null >> bootimg/01_dtbdump_gO"PW.dtb
+rm -f bootimg/01_dtbdump_gO"PW.dtb.* 2>/dev/null
+cat vendor/lib64/librelight_only.so.* 2>/dev/null >> vendor/lib64/librelight_only.so
+rm -f vendor/lib64/librelight_only.so.* 2>/dev/null
+cat system/system/lib/liblhdc.so.* 2>/dev/null >> system/system/lib/liblhdc.so
+rm -f system/system/lib/liblhdc.so.* 2>/dev/null
+cat system/system/data-app/MIMediaEditorGlobal/MIMediaEditorGlobal.apk.* 2>/dev/null >> system/system/data-app/MIMediaEditorGlobal/MIMediaEditorGlobal.apk
+rm -f system/system/data-app/MIMediaEditorGlobal/MIMediaEditorGlobal.apk.* 2>/dev/null
+cat system/system/app/MIUIGalleryPlayerService/MIUIGalleryPlayerService.apk.* 2>/dev/null >> system/system/app/MIUIGalleryPlayerService/MIUIGalleryPlayerService.apk
+rm -f system/system/app/MIUIGalleryPlayerService/MIUIGalleryPlayerService.apk.* 2>/dev/null
+cat system/system/app/Notes/Notes.apk.* 2>/dev/null >> system/system/app/Notes/Notes.apk
+rm -f system/system/app/Notes/Notes.apk.* 2>/dev/null
+cat system/system/app/MIUINotesPad/MIUINotesPad.apk.* 2>/dev/null >> system/system/app/MIUINotesPad/MIUINotesPad.apk
+rm -f system/system/app/MIUINotesPad/MIUINotesPad.apk.* 2>/dev/null
+cat system/system/lib64/liblhdc.so.* 2>/dev/null >> system/system/lib64/liblhdc.so
+rm -f system/system/lib64/liblhdc.so.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiFreeformService/MiuiFreeformService.apk.* 2>/dev/null >> system/system/priv-app/MiuiFreeformService/MiuiFreeformService.apk
+rm -f system/system/priv-app/MiuiFreeformService/MiuiFreeformService.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk.* 2>/dev/null >> system/system/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk
+rm -f system/system/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk.* 2>/dev/null
